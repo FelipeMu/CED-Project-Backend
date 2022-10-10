dae43251e4634ddadb916de703ac082c2d3b4376
Felipe <felipe.munoz.c@usach.cl>
2022-10-10 17:59:37 -0300

    Ft  conexión a db & cambiar nombre de tablas a minúsculas
    se logra conectar a la base de datos y mostrar la información.

--- demo/src/main/index.js
+++ demo/src/main/index.js
@@ -1,5 +1,27 @@
 const colors = require('colors');
+// Database Conection...
+const { Client } = require('pg') //controlador client
+
+//ejemplo conexion a base de datos
+const ObtenerProfesores = async () => {
+    //
+    const client = new Client({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'postgres',
+        password: '12345',
+        port: 5432,
+    });
+    
+    
+    await client.connect(); // Database Conection... clients ~ database
+    const res = await client.query("select * from profesor");
+    const result = res.rows[0].email;
+    await client.end();
+    return result;
+};
+
+ObtenerProfesores().then(res => {console.log(res)});
 
 
 
-console.log('Running'.magenta);
