965f046c35bcc9bf3caf96a27bf0a8e0ede09fc2
Felipe <felipe.munoz.c@usach.cl>
2022-11-05 18:42:35 -0300

    Ft atributo detalles petición
    .

--- demo/back2/src/controllers/index.controller.js
+++ demo/back2/src/controllers/index.controller.js
@@ -98,7 +98,7 @@ const putInformacionProfesor = async(req, res) =>{
 
 //OBTENER EVALUACIONES PARA MOSTRAR EN EL CALENDARIO
 const getEvaluaciones = async (req,res) => { 
-    const response = await pool.query('SELECT pg.nombre as name, pg.hora_inicio as start, pg.hora_termino as end, pg.detalles, (SELECT md.tipo_modalidad as modalidad FROM modalidad as md WHERE md.codigo = pg.id_modalidad), (SELECT te.tipo as tipo_evaluacion FROM tipo_evaluacion as te WHERE te.id = pg.id_tipo_evaluacion), pg.color FROM programar as pg WHERE pg.id_profesor_dicta = $1', [req.params.id_profesor]);
+    const response = await pool.query('SELECT pg.nombre as name, pg.hora_inicio as start, pg.hora_termino as end, pg.detalles as details, (SELECT md.tipo_modalidad as modalidad FROM modalidad as md WHERE md.codigo = pg.id_modalidad), (SELECT te.tipo as tipo_evaluacion FROM tipo_evaluacion as te WHERE te.id = pg.id_tipo_evaluacion), pg.color FROM programar as pg WHERE pg.id_profesor_dicta = $1', [req.params.id_profesor]);
     res.json(response.rows);
 };
 
