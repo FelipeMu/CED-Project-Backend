9833113be19e4f840c97a5eb092891c642faddb7
Felipe <felipe.munoz.c@usach.cl>
2022-11-09 17:11:21 -0300

    Ft se trae el código de los cursos

--- demo/back2/src/controllers/index.controller.js
+++ demo/back2/src/controllers/index.controller.js
@@ -43,7 +43,7 @@ const getAdministradores = async (req, res) => {
 };
 
 const getCursosById = async (req, res) => {
-    const response = await pool.query("SELECT DISTINCT pd.id, asig.nombre, asig.horas_semanales as hrs_semanal, pn.nivel, pd.seccion, md.tipo_modalidad as modalidad, '2/2022' as periodo FROM asignatura as asig, profesor_dicta as pd, plan_nivel as pn, modalidad as md, profesor as pr WHERE pd.id_profesor = $1 AND md.id = pd.id_modalidad AND asig.codigo = pd.codigo_asignatura AND asig.codigo = pn.codigo_asignatura", [req.params.id]);
+    const response = await pool.query("SELECT DISTINCT pd.id_profesor, pd.codigo_asignatura, asig.nombre, asig.horas_semanales as hrs_semanal, pn.nivel, pd.seccion, md.tipo_modalidad as modalidad, '2/2022' as periodo FROM asignatura as asig, profesor_dicta as pd, plan_nivel as pn, modalidad as md, profesor as pr WHERE pd.id_profesor = $1 AND md.id = pd.id_modalidad AND asig.codigo = pd.codigo_asignatura AND asig.codigo = pn.codigo_asignatura", [req.params.id]);
     res.json(response.rows);
 };
 
